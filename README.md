be432e0289f808d6678a05be3e394b147743bb0e
frew <daviddeveloper.frew@gmail.com>
2020-09-04 21:51:26 +0100

    starting point [video 4:49]

---
+++ README.md
@@ -0,0 +1,7 @@
+# rowsandcolumns
+
+Display a matrix made of rows and columns
+
+based on the following video
+
+https://www.youtube.com/watch?v=Fy5KzDwxjlA
---
+++ android/app/src/main/kotlin/com/frewconsulting/rowsandcolumns/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.frewconsulting.rowsandcolumns
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
